6874ab2efe8e86c4f39f360788b11b8783bbc7d9
erbertrotondano <erbertrotondano@gmail.com>
2024-09-11 16:01:04 -0300

    [refactor]: creating components for cta elements | [refactor]: preparing deploy stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  output: 'server',
+  adapter: netlify(),
 });
